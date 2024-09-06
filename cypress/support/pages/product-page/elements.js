3cdc40a84bbe75e5c2d33c6c6a3f11b0711e1808
Andrey Quagliato <andreyquagliato@gmail.com>
2024-09-06 20:38:30 -0300

    criacao de testes automatizados na página de visualização de produtos

---
+++ cypress/support/pages/product-page/elements.js
@@ -0,0 +1,8 @@
+// elementos da página para que não seja necessário reutilizar várias vezes o mesmo elemento
+
+export const ELEMENTS = {
+  productLabel: ".product_label",
+  filter: ".product_sort_container",
+  nameProductItem: ".inventory_item_name",
+  priceProductItem: ".inventory_item_price",
+};
